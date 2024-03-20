c6689b66ae57004cc9f130ba571427738cc61dd9
Radhi Rasho <Radhi.h@outlook.com>
2024-03-20 15:33:41 +0000

    stuff

---
+++ src/components/Card.tsx
@@ -0,0 +1,13 @@
+import type { User } from '@/types'
+import type { ReactNode } from 'react'
+
+export function Card({ user, children }: { user: User; children: ReactNode }) {
+    return (
+        <div>
+            {children}
+            <p className="text-random">
+                {user.name.first} {user.name.last}
+            </p>
+        </div>
+    )
+}
---
+++ src/components/Counter.tsx
@@ -0,0 +1,37 @@
+import type { User } from '@/types'
+import { useEffect, useState } from 'react'
+import { Card } from './Card'
+import { Image } from 'astro:assets'
+
+export default function Counter() {
+    const [users, setUsers] = useState<User[]>([])
+
+    async function getData() {
+        const res = await fetch('https://randomuser.me/api?results=25')
+        const data = await res.json()
+        setUsers(data.results)
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    return (
+        <>
+            <div>
+                {users.map((user) => {
+                    return (
+                        <Card user={user}>
+                            <Image
+                                width={50}
+                                height={50}
+                                src={user.picture.large}
+                                alt="user"
+                            />
+                        </Card>
+                    )
+                })}
+            </div>
+        </>
+    )
+}
